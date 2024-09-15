6fd452f26261b32c20b2650741d890cdbbb7e173
kangjk <96231886+KangJKJK@users.noreply.github.com>
2024-09-16 00:42:48 +0900

    Update network3.sh

--- network3.sh
+++ network3.sh
@@ -6,64 +6,43 @@ GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
 
-echo -e "${GREEN}Sonic 데일리 퀘스트 스크립트를 시작합니다...${NC}"
+echo -e "${GREEN}Network3 노드 설치를 시작합니다.${NC}"
 
-# 작업 디렉토리 설정
-work="/root/sonic-all"
+# 사용자에게 명령어 결과를 강제로 보여주는 함수
+req() {
+  echo -e "${YELLOW}$1${NC}"
+  shift
+  "$@"
+  echo -e "${YELLOW}결과를 확인한 후 엔터를 눌러 계속 진행하세요.${NC}"
+  read -r
+}
 
-# 기존 작업 디렉토리가 존재하면 삭제
-if [ -d "$work" ]; then
-    echo -e "${YELLOW}작업 디렉토리 '${work}'가 이미 존재하므로 삭제합니다.${NC}"
-    rm -rf "$work"
-fi
+# 홈 디렉토리로 이동합니다.
+cd $HOME
 
-# 파일 다운로드 및 덮어쓰기
-echo -e "${YELLOW}필요한 파일들을 다운로드합니다...${NC}"
+# 지정된 URL에서 ubuntu-node-v2.1.0.tar 파일을 다운로드합니다.
+wget https://network3.io/ubuntu-node-v2.1.0.tar && \
+tar -xf ubuntu-node-v2.1.0.tar && \
+rm -rf ubuntu-node-v2.1.0.tar && \
 
-# Git 설치
-echo -e "${YELLOW}Git을 설치합니다...${NC}"
-sudo apt install -y git
+# 압축 해제된 ubuntu-node 디렉토리로 이동합니다.
+cd ubuntu-node
 
-# 존재하는 파일을 삭제하고 다운로드
-echo -e "${YELLOW}Git 저장소 클론 중...${NC}"
-rm -rf ./*
-git clone https://github.com/KangJKJK/sonic-all
+# 노드를 실행합니다.
+sudo bash manager.sh up
 
-# 작업 디렉토리 이동
-echo -e "${YELLOW}작업디렉토리를 이동합니다...${NC}"
-cd "$work"
+# 노드의 개인키 및 본인의 IP를 표시합니다.
+req "노드의 개인키를 확인하시고 적어두세요." sudo bash manager.sh key
+IP_ADDRESS=$(curl -s ifconfig.me)
+req "사용자의 IP는 ${IP_ADDRESS}입니다." echo "사용자의 IP는 ${IP_ADDRESS}입니다."
 
-# npm 설치 여부 확인
-echo -e "${YELLOW}필요한 파일들을 설치합니다...${NC}"
-if ! command -v npm &> /dev/null; then
-    echo -e "${RED}npm이 설치되지 않았습니다. npm을 설치합니다...${NC}"
-    sudo apt-get update
-    sudo apt-get install -y npm
-else
-    echo -e "${GREEN}npm이 이미 설치되어 있습니다.${NC}"
-fi
-
-# Node.js 모듈 설치
-echo -e "${YELLOW}필요한 Node.js 모듈을 설치합니다...${NC}"
-npm install
-npm install @solana/web3.js chalk bs58
-
-# 개인키 입력받기
-read -p "Solana의 개인키를 쉼표로 구분하여 입력하세요: " privkeys
-
-# 개인키를 파일에 저장
-echo "$privkeys" > "$work/private.txt"
-
-# 파일 생성 확인
-if [ -f "$work/private.txt" ]; then
-    echo -e "${GREEN}개인키 파일이 성공적으로 생성되었습니다.${NC}"
-else
-    echo -e "${RED}개인키 파일 생성에 실패했습니다.${NC}"
-fi
-
-# sonic.js 스크립트 실행
-echo -e "${GREEN}sonic.js 스크립트를 실행합니다...${NC}"
-node --no-deprecation sonic.js
+# 웹계정과의 연동을 진행합니다.
+URL="https://account.network3.ai/main?o=${IP_ADDRESS}:8080"
+echo -e "${YELLOW}웹계정과 연동을 진행합니다.${NC}"
+echo -e "${YELLOW}다음 URL로 접속하세요: ${URL}${NC}"
+echo -e "${YELLOW}대시보드에서 현재 노드의 패널 우측 상단에 있는 +버튼을 클릭하여 노드의 개인키를 적어주세요.${NC}"
+echo -e "${YELLOW}계속 진행하려면 엔터를 눌러 주세요.${NC}"
+read -r
 
 echo -e "${GREEN}모든 작업이 완료되었습니다. 컨트롤+A+D로 스크린을 종료해주세요.${NC}"
 echo -e "${GREEN}스크립트 작성자: https://t.me/kjkresearch${NC}"
